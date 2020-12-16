0a192a78b1d6ded12e1effde457dfb938c60f51f
thewhitesummer <fe_granger@hotmail.com>
2020-12-16 14:01:52 -0300

    implemented movement on the players

---
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+
+public class UnitMovement : NetworkBehaviour {
+    
+    [SerializeField] private NavMeshAgent agent = null;
+
+    private Camera mainCamera;
+
+    #region Server
+
+    [Command]
+    private void CmdMove(Vector3 newPos) {
+        if(!NavMesh.SamplePosition(newPos, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
+        agent.SetDestination(hit.position);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        base.OnStartAuthority();
+        mainCamera = Camera.main;
+    }
+
+    [ClientCallback]
+    private void Update() {
+        if(!hasAuthority) { return; }
+        if(!Mouse.current.rightButton.wasPressedThisFrame) { return; }
+
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) { return; }
+
+        CmdMove(hit.point);
+    }
+
+    #endregion
+}
