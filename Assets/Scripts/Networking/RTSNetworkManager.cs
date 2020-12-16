b48abec0b227effb3ba9847704f69d63378793a8
thewhitesummer <fe_granger@hotmail.com>
2020-12-16 14:30:54 -0300

    spawning units when clicking on the spawner

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -4,5 +4,17 @@ using Mirror;
 using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager {
-    
+    [SerializeField] private GameObject unitSpawnerPrefab = null;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        GameObject unitSpawnerInstance = Instantiate(
+            unitSpawnerPrefab, 
+            conn.identity.transform.position, 
+            conn.identity.transform.rotation);
+
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 }
