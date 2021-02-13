b3496494fa87838a3cb4e39f34234143d4c82b20
Fernanda <fe_granger@hotmail.com>
2021-02-13 12:02:12 -0300

    made units targetable

---
+++ Assets/Scripts/Combat/Targetable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targetable : MonoBehaviour {
+    
+    [SerializeField] private Transform aimAtPoint = null;
+
+    public Transform GetAimAtPoint() {
+        return aimAtPoint;
+    }
+
+
+}
